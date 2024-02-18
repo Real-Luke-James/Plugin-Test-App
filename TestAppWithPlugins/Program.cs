174b7893c65b812a74192c62abfe1af2decb96f3
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2024-02-18 11:16:47 -0800

    Add Program Starting

--- TestAppWithPlugins/Program.cs
+++ TestAppWithPlugins/Program.cs
@@ -1,6 +1,47 @@
 ﻿using PluginBase;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+
+namespace TestAppWithPlugins
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (args.Length == 1 && args[0] == "/d") {
+					Console.WriteLine("Waiting for any key...");
+					Console.ReadLine();
+				}
+
+				// Load Commands from Plugins
+
+				if (args.Length == 0)
+				{
+					Console.WriteLine("Commands: ");
+					// Output commands
+				}
+				else
+				{
+					foreach (string commandName in args)
+					{
+						Console.WriteLine($"-- {commandName} --");
+
+						// Execute Command with name passed as argument
+
+						Console.WriteLine();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+		}
+	}
+}
