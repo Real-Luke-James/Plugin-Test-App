6d9de09430ce71ac1597ce774e4d22888791c407
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2024-02-18 12:46:00 -0800

    Add Hello Plugin Code

--- HelloPlugin/Class1.cs
+++ HelloPlugin/Class1.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using PluginBase;
+using System;
+
+namespace HelloPlugin
+{
+	public class HelloCommand : ICommand
+	{
+		public string Name { get => "hello"; }
+		public string Description { get => "Displays Hello Message."; }
+
+		public int Execute()
+		{
+			Console.WriteLine("Hello !!!");
+			return 0;
+		}
+	}
+}
